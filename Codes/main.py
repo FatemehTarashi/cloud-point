88fe7a6e71cc1f1b621db4412e432ecddcbb739d
Fatemeh Tarashi <tarashikashanifatemeh@gmail.com>
2023-07-09 09:37:17 +0330

    Update main.py

--- Codes/main.py
+++ Codes/main.py
@@ -6,12 +6,14 @@ from sklearn.cluster import DBSCAN
 from collections import Counter
 from scipy.spatial import ConvexHull
 
+
 from readAndPlot import plot_las_file, plot_npy_file
 from statistical_Outlier_Removal_filter import sor
 from groundDetection import threshold_height, ground_detection, non_ground_detection
 from DBSCAN import find_big_part
 from shadow import combined_with_shadow
 from volume import calculate_volume
+
 
 # CONFIG
 config = configparser.ConfigParser()
@@ -72,12 +74,41 @@ NAME =config.get('DATA','DATA_NAME') # Name of the data file
 
 ######
 
-mesh = pv.read('/home/Fatemeh/Task_Fatemetarashi/Exports/mesh.stl')
-volume = mesh.volume
-print(volume)
+#mesh = pv.read('/home/Fatemeh/Task_Fatemetarashi/Exports/mesh.stl')
+#volume = mesh.volume
+#print(volume)
+
 #mesh.plot(eye_dome_lighting=True, zoom = 3)
 #n=0.75
 #volume = calculate_volume(points,1-n)
 
 #with open('/home/Fatemeh/Task_Fatemetarashi/Exports/volume.txt', 'a') as f:
     #f.write(f"\nvolume{volume}  for {(n)*100}% height")
+
+points = plot_npy_file('/home/Fatemeh/Task_Fatemetarashi/Exports/big_part.npy', 'big_part')
+
+cloud = pv.PolyData(points)
+
+# Create a plane at the specified height (ground level)
+h =  threshold_height(cloud)
+ground_level = 10
+plane = pv.Plane(center=(cloud.center[0], cloud.center[1], h),
+                 direction=(0, 0, 1),
+                 i_size=cloud.length,
+                 j_size=cloud.length)
+
+# Extrude the point cloud to the ground plane
+extruded_cloud = cloud.extrude_trim((0, 0, -1), plane)
+
+# Visualize the original point cloud and the extruded surface
+p = pv.Plotter(shape=(1, 2))
+p.add_mesh(cloud, color='red')
+p.add_text('Original Cloud Point', position='upper_edge')
+
+p.subplot(0, 1)
+p.add_mesh(plane, style='wireframe', color='black')
+p.add_mesh(extruded_cloud, color='blue')
+p.add_text('Extruded one', position='upper_edge')
+
+p.link_views()
+p.show()
